4cb7a9ac99fd0dadc5e408dda4be959fd1ace06d
Barry Rowlingson <b.rowlingson@gmail.com>
2013-04-03 20:28:49 +0100

    list transform via SO answer, meta atts done right

--- R/signatures.R
+++ R/signatures.R
@@ -5,9 +5,11 @@ getSignatures <- function(id){
   response = getURL(url)
   json = fromJSON(response)
   sigs = json$signatures
-  m = ldply(sigs,function(s){t(data.frame(unlist(s)))})
-  atts=sigs[c("page","prev_page_endpoint","next_page_endpoint","total_pages","signature_count")]
+  non.null.list <- lapply(sigs, Filter, f = Negate(is.null))
+  m = rbind.fill(lapply(non.null.list, as.data.frame))
+  atts=json[c("page","prev_page_endpoint","next_page_endpoint","total_pages","signature_count")]
   attr(m,"meta")=atts
   m
   
 }
+
