6fb29c4e31d879319a129413468e648932e284ec
Barry Rowlingson <b.rowlingson@gmail.com>
2013-04-03 22:10:16 +0100

    docs, test api_key set

--- R/api.R
+++ R/api.R
@@ -1,3 +1,9 @@
+##' get the change.org API root URL
+##'
+##' Just a handy shortcut function for other funcitons
+##' @title getAPIroot URL
+##' @return the URL as a string
+##' @author Barry Rowlingson
 getAPIroot <- function(){
   return("https://api.change.org/v1/petitions/")
 }
--- R/key.R
+++ R/key.R
@@ -1,11 +1,28 @@
 
-# put the api key in the package enviroment
+##' Set the API Key
+##'
+##' You need an API key from www.change.org to use the API.
+##' 
+##' @title Set the API key
+##' @param key Hexadecimal key value
+##' @return nothing
+##' @author Barry Rowlingson
 setAPIkey <- function(key){
   e = ns_env("changer")
   assign("api_key",key,envir=e)
+  invisible(0)
 }
-
+##' Get the pre-set API key
+##'
+##' This function gets the API key from the package environment
+##' 
+##' @title Get the API Key
+##' @return the hexadecimal string previously set.
+##' @author Barry Rowlingson
 getAPIkey <- function(){
   e = ns_env("changer")
+  if(!exists("api_key",envir=e, inherits=FALSE)){
+    stop("No API key set with setAPIkey(...) function")
+  }
   get("api_key",envir=e,inherits=FALSE)
 }
