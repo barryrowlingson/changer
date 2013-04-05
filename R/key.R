8c86d71440960d099c37b3036c0f686d7250b332
Barry Rowlingson <b.rowlingson@lancs.ac.uk>
2013-04-05 15:17:19 +0100

    store the key in options

--- R/key.R
+++ R/key.R
@@ -9,23 +9,27 @@
 ##' @export
 ##' @author Barry Rowlingson
 setAPIkey <- function(key){
-  e = ns_env("changer")
-  assign("api_key",key,envir=e)
+  options(changer.key=key)
   invisible(0)
 }
 
 ##' Get the pre-set API key
 ##'
-##' This function gets the API key from the package environment
+##' This function gets the API key from options or environment
 ##' 
 ##' @title Get the API Key
 ##' @return the hexadecimal string previously set.
 ##' @export
 ##' @author Barry Rowlingson
 getAPIkey <- function(){
-  e = ns_env("changer")
-  if(!exists("api_key",envir=e, inherits=FALSE)){
+  option.key <- getOption("changer.key")
+  env.key <- Sys.getenv("CHANGER_KEY")
+  if (!is.null(option.key)){
+    return(option.key)
+  } else if (!env.key == "") {
+    return(env.key)
+  }else{
     stop("No API key set with setAPIkey(...) function")
   }
-  get("api_key",envir=e,inherits=FALSE)
+
 }
